9a251d70f5f9529dee110ef0bf7717e390260887
Daooshi <dominikwozniczka96@gmail.com>
2020-03-23 17:34:41 +0100

    Done with heron exercise

--- bai-master/02-JSCoreConcepts/heron.js
+++ bai-master/02-JSCoreConcepts/heron.js
@@ -1,11 +1,13 @@
 // boki trójkąta
-//a = 3;
+a = 3;
 b = 4;
 c = 5;
-h = 7
+h = 7; 
+
+formula = (a + b + c) / 2; // p - polowa obwodu trojkata potrzebna do obliczenia
 
 // Pole trókąta o bokach ..., ... oraz ... wynosi ... .
+calculation =  formula * ( formula - a ) * ( formula - b ) * ( formula - c );
+let sth = (a , b , c) =>  { return Math.sqrt(calculation) };
 
-let a = () => "aaaa";
-
-console.log( a() );
+console.log( "Pole trójkąta o bokach " + a + "," + b + "," + c + " wynosi: "+ sth(a , b , c) );
