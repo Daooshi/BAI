be36b737a9ca41d34ccfc9e2f9b34c0edb04c469
Daooshi <dominikwozniczka96@gmail.com>
2020-03-23 17:09:41 +0100

    Done with String exercise

--- bai-master/02-JSCoreConcepts/address.js
+++ bai-master/02-JSCoreConcepts/address.js
@@ -6,6 +6,19 @@ city = 'Kraków';
 zip = '30-781';
 country = 'Poland';
 
+// bez formatowania
+console.log(title);
+console.log(name);
+console.log(surname);
+console.log(street);
+console.log(zip);
+console.log(city);
+console.log(country);
+
+// z formatowaniem
+
+console.log("\n" + title + " " + name + " " + surname + "\n" + "ul. " + street  + "\n" + zip + " " + city + "\n" + country.toUpperCase())
+
 
 /* 
 dr Mateusz Kubicki
