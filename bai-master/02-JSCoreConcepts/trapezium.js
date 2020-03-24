5c43a8efae7f6f3b19c15010bf3cb51968a9b4ea
Daooshi <dominikwozniczka96@gmail.com>
2020-03-24 18:51:51 +0100

    comments added

--- bai-master/02-JSCoreConcepts/trapezium.js
+++ bai-master/02-JSCoreConcepts/trapezium.js
@@ -1,9 +1,10 @@
+//boki trapeza i ich wysokosc
 var a = 6;
 var b = 5;
 var h = 9;
 
-formula = ( a + b ) * h / 2;
+formula = ( a + b ) * h / 2; //wzor na pole trapezu
 
-let calculation = ( a, b, h ) => {return formula};
+let calculation = ( a, b, h ) => {return formula}; // kalkulacja
 
 console.log("Pole powierzchni trapezu wynosi: " + calculation( a, b, h ));
