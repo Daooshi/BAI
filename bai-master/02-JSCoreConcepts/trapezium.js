6f2c06828cf3b4eceb721a1535b2661d64d2f17a
Daooshi <dominikwozniczka96@gmail.com>
2020-03-24 18:15:20 +0100

    trapezium exercise done

---
+++ bai-master/02-JSCoreConcepts/trapezium.js
@@ -0,0 +1,9 @@
+var a = 6;
+var b = 5;
+var h = 9;
+
+formula = ( a + b ) * h / 2;
+
+let calculation = ( a, b, h ) => {return formula};
+
+console.log("Pole powierzchni trapezu wynosi: " + calculation( a, b, h ));
